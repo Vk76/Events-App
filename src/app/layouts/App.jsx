91ed4a3d083daa215c93b9840c6ced209c25c360
Vk76 <vishalkriplani56@gmail.com>
2020-10-05 19:25:18 +0530

    added navbar and styled it..!

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
-
+import { Container } from 'semantic-ui-react';
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";  
+import NavBar from "../../features/nav/NavBar/NavBar";
 function App() {
   return (
-    <div>Events App</div>
+    <div>
+      <NavBar />
+      <Container className = "main">
+        <EventDashboard />
+      </Container>
+    </div>
   );
 }
 
